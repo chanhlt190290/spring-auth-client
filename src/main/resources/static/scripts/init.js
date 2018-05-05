5fdc554b2eeb3758ee64fa89670bbd1e18d02a47
chanhlt <chanhlt190290@gmail.com>
2018-05-06 01:44:19 +0900

    fix bug not defined of function login, logout

--- src/main/resources/static/scripts/init.js
+++ src/main/resources/static/scripts/init.js
@@ -1,5 +1,5 @@
 var config = {
-
+  
   clientId:
     "682572018798-4rulmci9ecq7h6nmp2ljo3v3doomaq88.apps.googleusercontent.com",
   scopes: ["email", "profile", "https://www.googleapis.com/auth/calendar"],
@@ -11,8 +11,8 @@ firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    login(user);
+    if (typeof login != "undefined") login(user);
   } else {
-    logout();
+    if (typeof logout != "undefined") logout();
   }
 });
