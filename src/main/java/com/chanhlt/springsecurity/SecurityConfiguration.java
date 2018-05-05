c55c09b511cdccf8d5c7932bcb957c368c3471d9
chanhlt <chanhlt190290@gmail.com>
2018-05-06 01:16:00 +0900

    commit source 1st time

---
+++ src/main/java/com/chanhlt/springsecurity/SecurityConfiguration.java
@@ -0,0 +1,81 @@
+package com.chanhlt.springsecurity;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean 
+    public AuthenticationSuccessHandler authenticationSuccessHandler(){
+        final AuthenticationSuccessHandler handler = new AuthenticationSuccessHandler(){
+        
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                    Authentication authentication) throws IOException, ServletException {
+                response.sendRedirect("/");
+            }
+        };
+        return handler;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/login*").anonymous()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login").permitAll()
+                .successHandler(authenticationSuccessHandler())
+                .and()
+            .logout().permitAll().logoutSuccessUrl("/login");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/static/**", "/scripts/**");
+    }
+}
---
+++ src/main/resources/static/scripts/login.js
@@ -0,0 +1,69 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    login(user);
+  } else {
+    // User is signed out.
+  }
+});
+
+function login(user) {
+  firebase
+    .auth()
+    .currentUser.getIdToken(/* forceRefresh */ true)
+    .then(function(idToken) {
+      var form = document.createElement("form");
+      var element1 = document.createElement("input");
+      var element2 = document.createElement("input");
+
+      element1.style.visibility = "hidden";
+      element2.style.visibility = "hidden";
+
+      form.method = "POST";
+      form.action = "/login";
+
+      element1.value = idToken;
+      element1.name = "username";
+      form.appendChild(element1);
+
+      element2.value = user.email;
+      element2.name = "password";
+      form.appendChild(element2);
+
+      document.body.appendChild(form);
+
+      form.submit();
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+$(document).ready(function() {
+//   var user = firebase.auth().currentUser;
+
+//   if (user) {
+//     login(user);
+//   } else {
+//     // User is signed out.
+//   }
+  $("#loginBtn").click(function() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope(config.scopes.join(" "));
+    provider.clientID = config.clientId;
+    provider.discoveryDocs = config.discoveryDocs;
+    provider.apiKey = config.apiKey;
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        var user = result.user;
+        login(user);
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+      });
+  });
+});
